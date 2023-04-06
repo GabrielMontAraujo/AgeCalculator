35011f91165f5329ee7c48c6ad280a750dbbffc7
GabrielMontAraujo <graubx25@gmail.com>
2023-04-06 14:37:46 -0300

    update JavaScript

--- script.js
+++ script.js
@@ -9,18 +9,28 @@ function calculateAge() {
     months += 12
   }
   const days = Math.floor((today - birthDate) / (1000 * 60 * 60 * 24))
+  //=============================================================================================================
   //document.getElementById(
   //'result'
   //).innerHTML = `Sua idade é ${age} anos, ${months} meses e ${days} dias.`
+  //=============================================================================================================
+  console.log(birthDate)
 
-  // Seleciona todos os elementos label e armazena em uma variável
-  var labels = document.querySelectorAll('label')
+  /*if (typeof birthDate !== 'undefined' && birthDate !== null) {
+    //Se "birthDate" for uma data (do tipo Date), compará-la com "null" usando o operador "!==" não é a melhor forma de verificar se ela é indefinida ou não foi especificada. O correto seria usar o operador "typeof" em conjunto com a comparação com "undefined".
+  ===============================================================================================================*/
 
-  // Percorre a lista de labels e define a propriedade visibility para "visible"
-  for (var i = 0; i < labels.length; i++) {
-    labels[i].style.visibility = 'visible'
+  if (!isNaN(birthDate)) {
+    // verifica se birthDate é uma data válida
+    // Seleciona todos os elementos label e armazena em uma variável
+    var labels = document.querySelectorAll('label')
+
+    // Percorre a lista de labels e define a propriedade visibility para "visible"
+    for (var i = 0; i < labels.length; i++) {
+      labels[i].style.visibility = 'visible'
+    }
+    document.getElementById('anos').innerText = ` ${age} `
+    document.getElementById('meses').innerText = ` ${months} `
+    document.getElementById('dias').innerText = ` ${days} `
   }
-  document.getElementById('anos').innerText = ` ${age} `
-  document.getElementById('meses').innerText = ` ${months} `
-  document.getElementById('dias').innerText = ` ${days} `
 }
