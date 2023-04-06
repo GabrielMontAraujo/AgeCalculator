8f7e283d2ed8e60c1c0ca3f9d8fab2a8c5b95da8
GabrielMontAraujo <graubx25@gmail.com>
2023-04-06 12:50:18 -0300

    update Styling

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ function calculateAge() {
     months += 12
   }
   const days = Math.floor((today - birthDate) / (1000 * 60 * 60 * 24))
-  document.getElementById(
-    'result'
-  ).innerHTML = `Sua idade é ${age} anos, ${months} meses e ${days} dias.`
+  //document.getElementById(
+  //'result'
+  //).innerHTML = `Sua idade é ${age} anos, ${months} meses e ${days} dias.`
+
+  // Seleciona todos os elementos label e armazena em uma variável
+  var labels = document.querySelectorAll('label')
+
+  // Percorre a lista de labels e define a propriedade visibility para "visible"
+  for (var i = 0; i < labels.length; i++) {
+    labels[i].style.visibility = 'visible'
+  }
+  document.getElementById('anos').innerText = ` ${age} `
+  document.getElementById('meses').innerText = ` ${months} `
+  document.getElementById('dias').innerText = ` ${days} `
 }
